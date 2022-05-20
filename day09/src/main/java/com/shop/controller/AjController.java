e5cd37f2307712c5005249faf76ff7801c6c1dd1
earlyou <earlyou1@gmail.com>
2022-05-20 23:25:49 +0900

    2022.05.20 HTML5 day09

---
+++ day09/src/main/java/com/shop/controller/AjController.java
@@ -0,0 +1,104 @@
+package com.shop.controller;
+
+import java.sql.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AjController {
+	@RequestMapping("/ajax")
+	public String ajax(Model m) {
+		m.addAttribute("center","ajax/center");
+		m.addAttribute("left","ajax/left");
+		return "main";
+	}
+	
+	@RequestMapping("/register_formimpl")	// aj03
+	public String register_formimpl(Model m, String id, String pwd) {
+		System.out.println(id+" "+pwd);
+		m.addAttribute("center","ajax/registerok");
+		m.addAttribute("left","ajax/left");
+		m.addAttribute("rid",id);
+		return "main";
+	}
+	
+	@RequestMapping("/register_aj03")	// aj03
+	public String register_aj03(Model m, String id, String pwd, 
+			String name, Date dob, String gender, String email, String meal) {
+		System.out.println("id: "+id+"\npwd: "+pwd+"\nname: "+name+"\ndob: "+dob+
+							"\ngender: "+gender+"\nemail: "+email+"\nmeal: "+meal);
+		m.addAttribute("center","ajax/registerokaj03");
+		m.addAttribute("left","ajax/left");
+		m.addAttribute("id",id);
+		m.addAttribute("pwd",pwd);
+		m.addAttribute("name",name);
+		m.addAttribute("dob",dob);
+		m.addAttribute("gender",gender);
+		m.addAttribute("email",email);
+		m.addAttribute("meal",meal);
+		return "main";
+	}
+	
+	@RequestMapping("/aj01")
+	public String aj01(Model m) {
+		m.addAttribute("center","ajax/aj01");
+		m.addAttribute("left","ajax/left");
+		return "main";
+	}
+	@RequestMapping("/aj02")
+	public String aj02(Model m) {
+		m.addAttribute("center","ajax/aj02");
+		m.addAttribute("left","ajax/left");
+		return "main";
+	}
+	@RequestMapping("/aj03")
+	public String aj03(Model m) {
+		m.addAttribute("center","ajax/aj03");
+		m.addAttribute("left","ajax/left");
+		return "main";
+	}
+	@RequestMapping("/aj04")
+	public String aj04(Model m) {
+		m.addAttribute("center","ajax/aj04");
+		m.addAttribute("left","ajax/left");
+		return "main";
+	}
+	@RequestMapping("/aj05")
+	public String aj015(Model m) {
+		m.addAttribute("center","ajax/aj05");
+		m.addAttribute("left","ajax/left");
+		return "main";
+	}
+	@RequestMapping("/aj06")
+	public String aj06(Model m) {
+		m.addAttribute("center","ajax/aj06");
+		m.addAttribute("left","ajax/left");
+		return "main";
+	}
+	@RequestMapping("/aj07")
+	public String aj07(Model m) {
+		m.addAttribute("center","ajax/aj07");
+		m.addAttribute("left","ajax/left");
+		return "main";
+	}
+	@RequestMapping("/aj08")
+	public String aj08(Model m) {
+		m.addAttribute("center","ajax/aj08");
+		m.addAttribute("left","ajax/left");
+		return "main";
+	}
+	@RequestMapping("/aj09")
+	public String aj09(Model m) {
+		m.addAttribute("center","ajax/aj09");
+		m.addAttribute("left","ajax/left");
+		return "main";
+	}
+	@RequestMapping("/aj10")
+	public String aj10(Model m) {
+		m.addAttribute("center","ajax/aj10");
+		m.addAttribute("left","ajax/left");
+		return "main";
+	}
+}
